2d9d388de99e111900e8693df0142ddfb78ff33f
包伟斌 <1813148814@qq.com>
2024-12-02 17:01:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import './App.css'
+import { hexToHsl } from "@/utils"
+
+
+function App() {
+  const [hueAngle, setHueAngle] = useState<string>("0");
+  const [saturation, setSaturation] = useState<string>("0");
+  const [ligntness, setLigntness] = useState<string>("0");
+  const copy = () => {
+    navigator.clipboard.writeText(`hsl(${hueAngle} ${saturation}% ${ligntness}%)`)
+    alert("Copied")
+  }
+  const pickColor = () => {
+    //@ts-expect-error: EyeEyeDropper should be able to use;
+    if (!EyeDropper) {
+      alert("EyeDropper not supported");
+    }
+    //@ts-expect-error: EyeEyeDropper should be able to use;
+    const eyeDropper = new EyeDropper();
+    eyeDropper.open().then((result: { sRGBHex: string }) => {
+      setHueAngle(() => hexToHsl(result.sRGBHex).h.toString());
+      setSaturation(() => hexToHsl(result.sRGBHex).s.toString());
+      setLigntness(() => hexToHsl(result.sRGBHex).l.toString());
+    })
+  };
+
+  const setHsl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.name) {
+      case 'h':
+        setHueAngle(() => event.target.value);
+        break;
+      case 's':
+        setSaturation(() => event.target.value);
+        break;
+      case 'l':
+        setLigntness(() => event.target.value);
+        break;
+    }
+  }
+  return (
+    <>
+      <div className='center' style={{ zIndex: 1 }}>
+        <div className='flex fixed right-0 flex-col top-0'>
+          <label htmlFor="h">
+            Hue Angle:&nbsp;
+            <input type="range" min="0" max="359" value={hueAngle} name="h" onChange={setHsl} />
+          </label>
+          <label htmlFor="s">
+            Saturation:&nbsp;
+            <input type="range" name="s" value={saturation} onChange={setHsl} />
+          </label>
+          <label htmlFor="l">
+            Ligntness:&nbsp;
+            <input type="range" name="l" value={ligntness} onChange={setHsl} />
+          </label>
+          <div style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "1rem",
+          }}>
+            <button className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white" onClick={pickColor}>Pick</button>
+            <button className="rounded bg-green-400 hover:bg-green-600 py-2 px-4 text-white" onClick={copy}>Copy</button>
+            <button className="rounded bg-red-500 hover:bg-red-700 py-2 px-4 text-white" onClick={() => {
+              setHueAngle(() => Math.floor(Math.random() * 360).toString());
+              setSaturation(() => Math.floor(Math.random() * 100).toString());
+              setLigntness(() => Math.floor(Math.random() * 100).toString());
+            }}>Random</button>
+          </div>
+        </div>
+        <div className="h-[300px] w-[300px] rounded-full" style={{ "backgroundColor": `hsl(${hueAngle} ${saturation}% ${ligntness}%)` }}></div>
+      </div>
+      <div className="color-table"></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/index.ts
@@ -0,0 +1,53 @@
+
+const hexToRgb = (hex: string) => {
+  // 移除可能存在的井号 (#)
+  hex = hex.replace(/^#/, '');
+
+  // 处理不同长度的十六进制字符串
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+
+  return { r, g, b };
+};
+
+export const hexToHsl = (hex: string) => {
+  const { r, g, b } = hexToRgb(hex);
+  return rgbToHsl(r, g, b);
+};
+
+const rgbToHsl = (r: number, g: number, b: number) => {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  let h = 0;
+  let s = 0;
+  const l = (max + min) / 2;
+
+  if (max !== min) {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return {
+    h: Math.round(h * 360),
+    s: Math.round(s * 100),
+    l: Math.round(l * 100)
+  };
+};
